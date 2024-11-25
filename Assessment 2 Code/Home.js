7ba263fdd2fc12c211286b0ba35b8b46c9982dd3
kpl1ng <67187974+kpl1ng@users.noreply.github.com>
2024-11-25 10:34:15 +0000

    Add files via upload

---
+++ Assessment 2 Code/Home.js
@@ -0,0 +1,40 @@
+// const title = document.querySelector("h1");
+// const scroll = () => window.scrollY;
+// const background = document.querySelector(".titlebackground");
+// setInterval(function () { 
+//     console.log(scroll());
+//     if(scroll()<100){
+//         document.querySelector("h1").style.marginTop = (scroll()+300);
+//     }
+//     else{
+//         document.querySelector("h1").style.marginTop = 400;
+//     }
+//     console.log(document.querySelector("h1").style.marginTop)
+//   }, 10);
+
+function home(){
+    window.location.href="Home.html"
+}
+
+function about_us(){
+
+}
+
+function services(){
+
+}
+
+function faq(){
+    
+}
+
+function contact_us(){
+    console.log("contact us")
+}
+
+function mortgage(){
+    console.log("here")
+}
+
+
+
